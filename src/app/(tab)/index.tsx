d4980f0c3543da6aa97ac152d7aa4fcd64c4fde4
Yatik1 <yatiksrivastava1@gmail.com>
2024-09-19 17:15:29 +0530

    setup completed

---
+++ src/app/(tab)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>App Development Stats !! </Text>
+    </View>
+  )
+}
+
+export default index
