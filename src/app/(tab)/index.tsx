6083382dad187ae8c21638d16a6496be3ec0befb
Yatik1 <yatiksrivastava1@gmail.com>
2024-09-19 17:19:12 +0530

    dev

--- src/app/(tab)/index.tsx
+++ src/app/(tab)/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const index = () => {
   return (
     <View>
-      <Text>App Development Stats !! </Text>
+      <Text>App Development Starts !! </Text>
     </View>
   )
 }
