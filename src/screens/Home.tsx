5a63e7442d9fe2cbd824d2d70c7761ea0d40e395
Yatik1 <yatiksrivastava1@gmail.com>
2024-09-26 12:12:37 +0530

    fetching data

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -8,7 +8,18 @@ const Home = () => {
     const [categories , setCategories] = React.useState<Category[]>([])
     const [transactions , setTransactions] = React.useState<Transaction[]>([])
 
-    const db = useSQLiteContext()
+    const db = useSQLiteContext() 
+    
+    React.useEffect(() => {
+      db.withTransactionAsync(async () => {
+        await getData()
+      })
+    } , [db])
+
+    async function getData() {
+      const result = await db.getAllAsync(`SELECT * FROM Transactions`)
+      console.log(result)
+    }
 
   return (
     <View>
