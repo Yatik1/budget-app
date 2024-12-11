ad2bddf2c0b195bc38e20855b748e887aa7018c5
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-11 09:45:04 +0530

    home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,7 @@
 import { Category, Transaction, TransactionsByMonth } from '@/types'
 import { useSQLiteContext } from 'expo-sqlite'
 import * as React from 'react'
-import { ScrollView, Text } from 'react-native'
+import { ScrollView, StyleSheet, Text, TextStyle } from 'react-native'
 import TransactionsList from '../components/TransactionsList'
 import Card from '../components/ui/Card'
 
@@ -84,13 +84,58 @@ function TransactionSummary({totalExpenses,totalIncome} : TransactionsByMonth) {
     year:"numeric"
   })
 
+  const getMoneyTextStyle = (value:number) : TextStyle => ({
+    fontWeight:"bold",
+    color:value<0 ? "#ff4500" : "#2e8b57"
+  })
+
+  const formatMoney = (value:number) => {
+    const absValue = Math.abs(value).toFixed(2)
+    return `${value < 0 ? "-" : ""}$${absValue}`
+  }
+
   return (
-    <Card style={{marginBottom:20}}>
-      <Text>Summary for {readablePeriod}</Text>
+    <Card style={styles.container}>
+      <Text style={styles.periodTitle}>Summary for {readablePeriod}</Text>
+      <Text style={styles.summaryText}>
+          Income:{" "}
+          <Text style={getMoneyTextStyle(totalIncome)}>
+            {formatMoney(totalIncome)}
+          </Text>
+      </Text>
+      <Text style={styles.summaryText}>
+          Total Expenses:{" "}
+          <Text style={getMoneyTextStyle(totalExpenses)}>
+            {formatMoney(totalExpenses)}
+          </Text>
+      </Text>
+      <Text style={styles.summaryText}>
+        Savings: {" "}
+        <Text style={getMoneyTextStyle(saving)}>
+          {formatMoney(saving)}
+        </Text>
+      </Text>
     </Card>
   )
 }
 
+const styles = StyleSheet.create({
+  container:{
+    marginBottom:15,
+    paddingBottom:7
+  },
+  periodTitle:{
+    fontSize:20,
+    fontWeight:"bold",
+    color:"#333",
+    marginBottom:15
+  },
+  summaryText:{
+    fontSize:18,
+    color:"#333",
+    marginBottom:10
+  }
+})
 
 export default Home
 
